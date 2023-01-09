aa7fc666daf63e3aecc954fe29e11d1113c25234
anita_oliveira211 <anitagarciaoliveira@gmail.com>
2023-01-08 22:27:00 -0300

    Auteração teste 2

--- test_oito_rainhas.py
+++ test_oito_rainhas.py
@@ -8,6 +8,15 @@ jogo_invalido_tamanho = ['00000001',
                  '00000001',
                  '00000100']
 
+jogo_invalido_linha = ['00000001',
+               '01000000',
+               '00010000',
+               '00000010',
+               '00100000',
+               '00000001',
+               '00000100',
+               '1000000']
+
 jogo_valido_posicao_falha = ['00000001',
                              '01000000',
                              '00010000',
@@ -20,5 +29,5 @@ jogo_valido_posicao_falha = ['00000001',
 def test_quando_tabuleiro_recebe_jogo_invalido_tamanho_deve_retornar_menos_1():
     assert tabuleiro(jogo_invalido_tamanho)==-1
 
-def test_jogo_valido_posicao_falha():
-    assert tabuleiro(jogo_valido_posicao_falha)==0
+def test_quando_tabuleiro_recebe_jogo_invalido_linha_deve_retornar_menos_1():
+    assert tabuleiro(jogo_invalido_linha)==-1
