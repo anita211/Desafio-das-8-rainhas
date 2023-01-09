81b6d760661bfea12e449467789a18a05c1c0bda
anita_oliveira211 <anitagarciaoliveira@gmail.com>
2023-01-08 22:47:44 -0300

    Teste 6: diagonal

--- test_oito_rainhas.py
+++ test_oito_rainhas.py
@@ -44,6 +44,15 @@ jogo_falho_coluna = ['00001000',
                '00000100',
                '10000000']
 
+jogo_falho_diagonal = ['00000001',
+               '00000010',
+               '00000100',
+               '00001000',
+               '00010000',
+               '00100000',
+               '01000000',
+               '10000000']
+
 def test_quando_tabuleiro_recebe_jogo_invalido_tamanho_deve_retornar_menos_1():
     assert tabuleiro(jogo_invalido_tamanho)==-1
 
@@ -58,3 +67,6 @@ def test_quando_tabuleiro_recebe_jogo_falho_linha_deve_retornar_0():
 
 def test_quando_tabuleiro_recebe_jogo_falho_coluna_deve_retornar_0():
     assert tabuleiro(jogo_falho_coluna)==0
+
+def test_quando_tabuleiro_recebe_jogo_falho_diagonal_deve_retornar_0():
+    assert tabuleiro(jogo_falho_diagonal)==0
