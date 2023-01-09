40dc9345b02a587c6f48b856a11165a35588502a
anita_oliveira211 <anitagarciaoliveira@gmail.com>
2023-01-08 22:39:28 -0300

    Resolve Teste 5

--- oito_rainhas.py
+++ oito_rainhas.py
@@ -12,10 +12,28 @@ def tabuleiro(matriz):
             return False
         return True
 
+    def Matriz_Transversa(matriz):
+        result = [[],[],[],[],[],[],[],[]]
+        for linha in matriz:
+            j = 0
+            for elemento in linha:
+                result[j].append(elemento)
+                j += 1
+        for n in range(8):
+            string = ''.join(map(str, result[n]))
+            result[n] = string
+        return result
+
     if not Valida_Formato(matriz):
         return -1
     
     for linha in matriz:
+        if linha.count('1') != 1:
+            return 0
+    
+    matriz_t = Matriz_Transversa(matriz)
+    
+    for linha in matriz_t:
         if linha.count('1') != 1:
             return 0
     
