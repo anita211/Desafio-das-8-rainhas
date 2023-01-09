4630fd47497ca324396412654fd206b5aed576a8
anita_oliveira211 <anitagarciaoliveira@gmail.com>
2023-01-08 22:18:40 -0300

    Auteração em Teste 1 e Teste 2

--- oito_rainhas.py
+++ oito_rainhas.py
@@ -1,2 +1,13 @@
-def tabuleiro(argumento):
-    return -1
+def tabuleiro(matriz):
+
+    def Valida_Formato(matriz):
+        return True
+
+    if not Valida_Formato(matriz):
+        return -1
+    
+    for linha in matriz:
+        if linha.count('1') != 1:
+            return 0
+    
+    return 1
--- test_oito_rainhas.py
+++ test_oito_rainhas.py
@@ -1,4 +1,18 @@
 from oito_rainhas import tabuleiro
 
-def test_quando_tabuleiro_recebe_1_deve_retornar_menos_1():
-    assert tabuleiro(1)==-1
+jogo_invalido = [1]
+
+jogo_valido_posicao_falha = ['00000001',
+                             '01000000',
+                             '00010000',
+                             '00000010',
+                             '00100000',
+                             '00000001',
+                             '00000100',
+                             '10000000']
+
+def test_quando_tabuleiro_recebe_jogo_invalido_deve_retornar_menos_1():
+    assert tabuleiro(jogo_invalido)==-1
+
+def test_jogo_valido_posicao_falha():
+    assert tabuleiro(jogo_valido_posicao_falha)==0
