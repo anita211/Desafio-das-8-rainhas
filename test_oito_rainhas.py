42d0eb16628e87ec4596dcea2ed22b92b1e64cc8
anita_oliveira211 <anitagarciaoliveira@gmail.com>
2023-01-08 22:34:59 -0300

    Resolve teste 4

--- test_oito_rainhas.py
+++ test_oito_rainhas.py
@@ -26,14 +26,14 @@ jogo_invalido_numero_de_rainhas = ['00000001',
                '00000100',
                '10000001']
 
-jogo_valido_posicao_falha = ['00000001',
-                             '01000000',
-                             '00010000',
-                             '00000010',
-                             '00100000',
-                             '00000001',
-                             '00000100',
-                             '10000000']
+jogo_falho_numero_de_rainhas = ['01001000',
+               '00000000',
+               '00010000',
+               '00000010',
+               '00100000',
+               '00000001',
+               '00000100',
+               '10000000']
 
 def test_quando_tabuleiro_recebe_jogo_invalido_tamanho_deve_retornar_menos_1():
     assert tabuleiro(jogo_invalido_tamanho)==-1
@@ -43,3 +43,6 @@ def test_quando_tabuleiro_recebe_jogo_invalido_linha_deve_retornar_menos_1():
 
 def test_quando_tabuleiro_recebe_jogo_invalido_numero_de_rainhas_deve_retornar_menos_1():
     assert tabuleiro(jogo_invalido_numero_de_rainhas)==-1
+
+def test_quando_tabuleiro_recebe_jogo_falho_numero_de_rainhas_deve_retornar_0():
+    assert tabuleiro(jogo_falho_numero_de_rainhas)==0
